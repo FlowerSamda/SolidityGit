98d119096e61f7f31b0db82bded3c379ad067f7e
FlowerSamda <flowersamda@gmail.com>
2021-04-22 20:49:00 +0900

    STiger 토큰 구현:

--- Autofarm/AUTOv2.sol
+++ Autofarm/AUTOv2.sol
@@ -5,8 +5,8 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.12;
-
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
 abstract contract Context {
     function _msgSender() internal view virtual returns (address payable) {
         return msg.sender;
